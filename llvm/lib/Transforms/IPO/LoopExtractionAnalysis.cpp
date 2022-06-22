8c6a7150c4f6667c8cce32028cb9029863e46e98
Vir Narula <virsvirus@gmail.com>
2022-06-22 08:56:33 -0700

    Add basic loop extraction functionality

--- llvm/lib/Transforms/IPO/LoopExtractionAnalysis.cpp
+++ llvm/lib/Transforms/IPO/LoopExtractionAnalysis.cpp
@@ -1,6 +1,11 @@
 #include "llvm/Analysis/AssumptionCache.h"
 #include "llvm/Analysis/LoopInfo.h"
+
 #include "llvm/IR/Dominators.h"
+
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/CodeExtractor.h"
+
 #include "llvm/Transforms/IPO/LoopExtractionAnalysis.h"
 
 using namespace llvm;
@@ -20,6 +25,11 @@ namespace {
     bool runOnModule(Module &M);
 
 private:
+  bool runOnFunction(Function &F);
+
+  Function *ExtractLoop(Loop *L, LoopInfo &LI, DominatorTree &DT);
+  SmallVector<Function*, 16> ExtractedLoops;
+
   function_ref<DominatorTree &(Function &)> LookupDomTree;
   function_ref<LoopInfo &(Function &)> LookupLoopInfo;
   function_ref<AssumptionCache *(Function &)> LookupAssumptionCache;
@@ -43,6 +53,56 @@ PreservedAnalyses LoopExtractionAnalysisPass::run(Module &M, ModuleAnalysisManag
   return PreservedAnalyses::all();
 }
 
+Function *LoopExtractionAnalyzer::ExtractLoop(Loop *L, LoopInfo &LI, DominatorTree &DT) {
+  Function &Func = *L->getHeader()->getParent();
+  AssumptionCache *AC = LookupAssumptionCache(Func);
+  CodeExtractorAnalysisCache CEAC(Func); 
+  CodeExtractor Extractor(DT, *L, false, nullptr, nullptr, AC);
+
+  if (Function *ExtractionLoop = Extractor.extractCodeRegion(CEAC)) {
+    ExtractedLoops.push_back(ExtractionLoop);
+    LI.erase(L);
+  }
+
+  return nullptr;
+}
+
+bool LoopExtractionAnalyzer::runOnFunction(Function &F) {
+  if (F.empty())
+    return false;
+
+  LoopInfo &LI = LookupLoopInfo(F);
+  DominatorTree &DT = LookupDomTree(F);
+  
+  if (LI.empty()) {
+    return false;
+  }
+
+  SmallVector<Loop*, 8> Loops;
+  Loops.assign(LI.begin(), LI.end());
+  for (Loop *L : Loops) {
+    if (L->isLoopSimplifyForm()) {
+      ExtractLoop(L, LI, DT);
+    }
+  }
+
+  return false;
+}
+
 bool LoopExtractionAnalyzer::runOnModule(Module &M) {
-  return true;
+  std::unique_ptr<Module> ClonedModPtr = CloneModule(M);
+
+  auto End = --ClonedModPtr->end();
+  for (auto Iter = ClonedModPtr->begin(); ; ++Iter) {
+    runOnFunction(*Iter);
+    if (Iter == End) {
+      break;
+    }
+  }
+
+  for (auto Iter = ExtractedLoops.begin(); ExtractedLoops.end(); ++Iter) {
+    errs() << (*Iter)->getName() << "\n";
+  }
+
+  return false;
 }
