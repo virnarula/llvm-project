fff0b69a90817e0d0861c24c2f0edcd738085c6a
Paul Walker <paul.walker@arm.com>
2022-10-20 11:53:58 +0000

    [NFC] Fix a few whitespace inconsistencies.

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -205,7 +205,6 @@ PipelineTuningOptions::PipelineTuningOptions() {
 }
 
 namespace llvm {
-
 extern cl::opt<unsigned> MaxDevirtIterations;
 extern cl::opt<bool> EnableConstraintElimination;
 extern cl::opt<bool> EnableFunctionSpecialization;
@@ -222,14 +221,10 @@ extern cl::opt<bool> EnableDFAJumpThreading;
 extern cl::opt<bool> RunNewGVN;
 extern cl::opt<bool> RunPartialInlining;
 extern cl::opt<bool> ExtraVectorizerPasses;
-
 extern cl::opt<bool> FlattenedProfileUsed;
-
 extern cl::opt<AttributorRunOption> AttributorRun;
 extern cl::opt<bool> EnableKnowledgeRetention;
-
 extern cl::opt<bool> EnableMatrix;
-
 extern cl::opt<bool> DisablePreInliner;
 extern cl::opt<int> PreInlineThreshold;
 } // namespace llvm
